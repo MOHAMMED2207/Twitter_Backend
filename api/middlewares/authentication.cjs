e65d4214a54f852dc8eeb19071ac613d96508233
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-07-22 21:33:53 +0300

    Update authentication.cjs

--- api/middlewares/authentication.cjs
+++ api/middlewares/authentication.cjs
@@ -3,8 +3,8 @@ const dotenv = require("dotenv");
 
 module.exports = async (req, res, next) => {
   dotenv.config();
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmdWxsbmFtZSI6Ik1vaGFtbWVkIEFiZHVsIEZhdGFoIiwidXNlcm5hbWUiOiJNb2hhbW1lZF9PaWZmaWNhbCIsIlBhc3N3b3JkIjoiJDJiJDEwJGhQZjUvcHUuQUJRWUVYQW5aL21Bci5QaFV6Q0hoWFVKRGpKLnFpVHVJWXplQmJ0bGtlaDkuIiwiZW1haWwiOiJtb29tdWRpMjFAZ21haWwuY29tIiwiUGhvbmUiOiIwMTE1MjIzNzcxMiIsImFnZSI6MjIsImNpdHkiOiJDYWlybyIsIl9pZCI6IjY2NDIyMjhlYmY3OWRiZTRkOTNjYWRjZCIsImlhdCI6MTcyMTY2NTI4NH0.8_ikVDrqUzwxOMGCed6p2SwVIDZfjgXmqvBw0Ih1uLU"
-    req.cookies.jwt;
+  const token =  req.cookies.jwt;
+    // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmdWxsbmFtZSI6Ik1vaGFtbWVkIEFiZHVsIEZhdGFoIiwidXNlcm5hbWUiOiJNb2hhbW1lZF9PaWZmaWNhbCIsIlBhc3N3b3JkIjoiJDJiJDEwJGhQZjUvcHUuQUJRWUVYQW5aL21Bci5QaFV6Q0hoWFVKRGpKLnFpVHVJWXplQmJ0bGtlaDkuIiwiZW1haWwiOiJtb29tdWRpMjFAZ21haWwuY29tIiwiUGhvbmUiOiIwMTE1MjIzNzcxMiIsImFnZSI6MjIsImNpdHkiOiJDYWlybyIsIl9pZCI6IjY2NDIyMjhlYmY3OWRiZTRkOTNjYWRjZCIsImlhdCI6MTcyMTY2NTI4NH0.8_ikVDrqUzwxOMGCed6p2SwVIDZfjgXmqvBw0Ih1uLU"
   console.log(token);
   try {
     if (!token) return res.status(403).json({ error: "Access Denied" });
